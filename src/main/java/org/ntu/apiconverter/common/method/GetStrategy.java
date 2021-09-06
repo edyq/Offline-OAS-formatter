236286acdb1b343273e35a54a90def2f47c0a5e0
PLC0114 <44118539+PLC0114@users.noreply.github.com>
2021-09-07 01:46:07 +0800

    [ADD] [WIP] response format

--- src/main/java/org/ntu/apiconverter/common/method/GetStrategy.java
+++ src/main/java/org/ntu/apiconverter/common/method/GetStrategy.java
@@ -4,9 +4,19 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class GetStrategy implements MethodTypeStrategy{
+    private List<String> supportedResponseType;
+
+    public GetStrategy(){
+        supportedResponseType = new ArrayList<>();
+        supportedResponseType.add("application/json");
+        supportedResponseType.add("application/x-www-form-urlencoded");
+    }
+
     @Override
     public JSONObject construct(Document content) {
         // get common ones: url param, path param(temporarily ignore)
@@ -21,6 +31,8 @@ public class GetStrategy implements MethodTypeStrategy{
         } else{
             jsonObject.put("description","request without urlParam");
         }
+
+        jsonObject.put("responses",formatResponses((Document)content.get("response")));
         return jsonObject;
     }
 
@@ -33,6 +45,23 @@ public class GetStrategy implements MethodTypeStrategy{
         schema.put("example",entry.getValue());
         jsonObject.put("schema",schema);
 
+        return jsonObject;
+    }
+
+    protected JSONObject formatResponses(Document response){
+        JSONObject jsonObject = new JSONObject();
+        JSONObject responseContent = new JSONObject();
+        responseContent.put("description","response with code "+response.get("status_code"));
+
+
+//        if (supportedResponseType.indexOf(response.get("content-type")) != -1){
+//            JSONObject content = new JSONObject();
+//            for (response.)
+//            JSONObject schema = new JSONObject();
+//        }
+//
+//        responseContent.put("content",content);
+//        jsonObject.put(response.get("status_code"),);
         return jsonObject;
     }
 }
